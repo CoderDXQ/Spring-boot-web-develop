0637273651da960c77daa32f720a8cfdc3016028
cococolabear <794055465@qq.com>
2020-08-03 02:23:00 +0800

    add_thymeleaf

--- src/main/java/com/atguigu/springboot/controller/HelloController.java
+++ src/main/java/com/atguigu/springboot/controller/HelloController.java
@@ -4,11 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
+import java.util.Map;
+
 @Controller
 public class HelloController {
     @ResponseBody
     @RequestMapping("/hello")
     public String hello(){
         return "Hello World";
+    }
+
+    @RequestMapping("/success")
+    public String success(Map<String,Object> map){
+
+        //classpath:/template/success.html
+        map.put("hello","你好");
+        map.put("users", Arrays.asList("zhangsan","lisi","wangwu"));
+        return "success";
     }
 }
