33f34cfb5905326a4c4e97b1c215c02b36bfbe58
cococolabear <794055465@qq.com>
2020-08-03 18:25:09 +0800

    add_importresource

--- src/main/java/com/atguigu/springboot/config/MyMvcConfig.java
+++ src/main/java/com/atguigu/springboot/config/MyMvcConfig.java
@@ -1,11 +1,11 @@
 package com.atguigu.springboot.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@EnableWebMvc //这个注解可以全面接管Spring MVC
+//@EnableWebMvc //这个注解可以全面接管Spring MVC，然后就不能自动访问静态资源下的index.html
 @Configuration
 public class MyMvcConfig extends WebMvcConfigurerAdapter {
 
@@ -17,4 +17,18 @@ public class MyMvcConfig extends WebMvcConfigurerAdapter {
         //这个配置直接去templates文件夹下找success.html，而不走controller文件夹下的逻辑文件
         registry.addViewController("/atguigu").setViewName("success");
     }
+
+    //@Bean之后所有的WebMvcConfigurerAdapter都会一起起作用
+    @Bean
+    public WebMvcConfigurerAdapter webMvcConfigurerAdapter(){
+        WebMvcConfigurerAdapter adapter = new WebMvcConfigurerAdapter(){
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName("login");
+                registry.addViewController("/index.html").setViewName("login");
+            }
+        };//匿名内部类必须有分号
+        return adapter;
+    }
+
 }
