fdbb2ae6be7bd191977d42420a9ccd73d5430ac3
cococolabear <794055465@qq.com>
2020-08-04 14:53:38 +0800

    员工添加页面完成

--- src/main/java/com/atguigu/springboot/controller/EmployeeController.java
+++ src/main/java/com/atguigu/springboot/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collection;
 
@@ -38,5 +39,18 @@ public class EmployeeController {
         Collection<Department> departments = departmentDao.getDepartments();
         model.addAttribute("depts",departments);
         return "emp/add";
+    }
+
+    //员工添加
+    //SpringMVC自动将请求参数和入参对象的属性进行一一绑定，要求请求参数的名字和javaBean入参的对象里面的属性名一致
+    //在页面提交数据时要注意数据的格式,格式错误会报400的状态码
+    @PostMapping("/emp")
+    public String addEmp(Employee employee){
+        System.out.println("保存的员工信息："+employee);
+        //保存员工数据到数据库
+        employeeDao.save(employee);
+        //来到员工列表页面
+        //redirect是重定向，forward是转发
+        return "redirect:/emps"; //地址中的"/"代表当前项目路径
     }
 }
